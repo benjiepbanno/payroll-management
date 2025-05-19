eb756cd6ca3b35416e034a7807d284b37ddcdafe
benjiepbanno <benjiepbanno@gmail.com>
2025-05-19 13:59:31 +0800

    feat: enhance payroll register header; add error handling for missing check details and claimant

--- components/payroll-registers/payroll-register-summary/header.tsx
+++ components/payroll-registers/payroll-register-summary/header.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { CheckDetails } from "@/lib/payroll-registers/types";
 import { formatAmount } from "@/lib/payroll-registers/utils";
 
@@ -16,16 +17,29 @@ export default function PayrollRegisterSummaryHeader({
     <div>
       <div className="font-bold text-center">General Payroll Register</div>
       <div className="font-bold text-center">Summary</div>
+
       <div className="text-sm text-center">
         <span className="font-bold">Check Details </span>
-        <span>Chck#: {check_details.number} </span>
-        <span>Dated: {check_details.date} </span>
-        <span>({formatAmount(check_details.amount)})</span>
+        {check_details.number && check_details.date && check_details.amount ? (
+          <>
+            <span>Chck#: {check_details.number} </span>
+            <span>Dated: {check_details.date} </span>
+            <span>({formatAmount(check_details.amount)})</span>
+          </>
+        ) : (
+          <Badge variant="destructive">not found</Badge>
+        )}
       </div>
 
       <div className="text-sm">
-        <span className="font-bold">Claimant:</span> {claimant}
+        <span className="font-bold">Claimant: </span>
+        {claimant ? (
+          <>{claimant}</>
+        ) : (
+          <Badge variant="destructive">not found</Badge>
+        )}
       </div>
+
       <div className="text-sm">
         <span className="font-bold">Reference Number:</span> {reference_number}
       </div>
